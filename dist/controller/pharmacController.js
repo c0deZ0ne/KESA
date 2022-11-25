7030c409fe52e32d1e5ee5c94cbc7a2ba228a78e
dogubo mamuzo joshua <51678953+c0deZ0ne@users.noreply.github.com>
2022-11-25 06:56:14 +0100

    Delete dist directory

--- dist/controller/pharmacController.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.searchPhamacy = void 0;
-const searchPhamacy = async (req, res) => {
-    res.render("search-phamacy");
-};
-exports.searchPhamacy = searchPhamacy;
--- dist/routes/Admin.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// import express from "express";
-// import {auth} from '../middleware/authorization'
-// import { AdminRegister } from "../controller/adminController";
-// const router = express.Router();
-// router.post('/create-admin', auth, AdminRegister)
-// // router.post('/create-super-admin',SuperAdmin)
-// export default router;
